cb2b7bdb26bc51e88c83864a60bc65c6c0bfd45f
Syltenor <74373766+Syltenor@users.noreply.github.com>
2021-05-28 23:35:45 +0200

    ModuleJoueur doc

--- ModuleJoueurs.vb
+++ ModuleJoueurs.vb
@@ -1,6 +1,8 @@
 ﻿Module ModuleJoueurs
     Private joueurs As List(Of Joueur)
     Private nomFile = "noms.txt"
+
+    'Sub qui enregistre une nouvelle entrée dans les joueurs. Vérifie si le joueur existe déjà. Si oui, son compteur de parties est actualisé, sinon, un nouveau joueur est créé
     Public Sub NouvelleEntree(nom As String)
         Dim joueurEntre = joueurs.FindIndex(Function(j As Joueur) j.GetNom() = nom)
         If joueurEntre <> -1 Then
@@ -10,6 +12,9 @@
         End If
     End Sub
 
+    'Fonction qui renvoie un joueur dont le nom correspond à celui entré en paramètre
+    '@param nom : String Le nom du joueur recherché
+    '@return Joueur Le joueur dont le nom correspond au paramères (Nothing si introuvable)
     Public Function getJoueur(nom As String) As Joueur
         For Each joueur As Joueur In joueurs
             If joueur.GetNom = nom Then
@@ -20,10 +25,13 @@
 
     End Function
 
+    'Fonction qui renvoie le nom du fichier de sauvegarde des joueurs
+    '@return String Le nom de fichier de sauvegarde des joueurs
     Public Function getNomFile()
         Return nomFile
     End Function
 
+    'Sub qui initialise le tableau contenant les valeurs des statistiques de tous les joueurs enregistrés dans le fichier
     Public Sub initJoueurs()
         joueurs = New List(Of Joueur)
         Dim sFile = My.Computer.FileSystem.ReadAllText(nomFile)
@@ -35,6 +43,8 @@
         Next
     End Sub
 
+    'Fonction qui renvoie une liste de tous les noms des joueurs existants
+    '@return List(Of String) Liste de tous les noms des joueurs enregistrés
     Public Function getNomsJoueurs()
         Dim noms As List(Of String)
         noms = New List(Of String)
@@ -44,6 +54,7 @@
         Return noms
     End Function
 
+    'Sub qui sauvegarde les joueurs contenus dans le tableau contenant tous les joueurs vers le fichier de sauvegarde
     Public Sub saveJoueurs()
         Dim strFile As String
         Dim txtJoueur As String
@@ -55,6 +66,8 @@
         My.Computer.FileSystem.WriteAllText(nomFile, strFile, False)
     End Sub
 
+    'Fonction qui retourne toutes les informations des joueurs
+    '@return List(Of Joueur) Liste des informations de tous les joueurs
     Public Function getAllJoueurs()
         Return joueurs
     End Function
