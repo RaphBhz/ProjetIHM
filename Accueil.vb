eeea3877e1ba35836b4cdf66146bec2d125c6686
Syltenor <74373766+Syltenor@users.noreply.github.com>
2021-05-28 23:24:09 +0200

    doc accueil

--- Accueil.vb
+++ Accueil.vb
@@ -1,10 +1,13 @@
 ﻿Public Class Accueil
+
+    'Evènement suivant le clic sur bouton quitter qui arrête l'application et la ferme
     Private Sub BtnQuitter_Click(sender As Object, e As EventArgs) Handles btnQuitter.Click
         If MsgBox("Fermer l'application ?", vbYesNo, "Quitter") = vbYes Then
             Accueil.ActiveForm.Close()
         End If
     End Sub
 
+    'Evènement suivant le clic sur bouton jouer qui ouvre la fenêtre de jeu de Memory
     Private Sub BtnJouer_Click(sender As Object, e As EventArgs) Handles btnJouer.Click
         If cbxNom.Text.Length < 3 Then
             MsgBox("Entrez un nom d'une longueur minimum de 3 caractères.", vbOKOnly, "Erreur")
@@ -20,6 +23,7 @@
         End If
     End Sub
 
+    'Evènement suivant le chargement de la fenêtre d'accueil
     Private Sub Accueil_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         initJoueurs()
         cbxNom.Items.Clear()
@@ -31,10 +35,12 @@
         Next
     End Sub
 
+    'Evènement suivant la fermeture de la fenêtre d'accueil qui sauvegarde les données de chaque joueur
     Private Sub Accueil_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         saveJoueurs()
     End Sub
 
+    'Evènement suivant le clic sur le bouton score qui ouvre la fenêtre de consultation des scores et cache l'accueil
     Private Sub btnScores_Click(sender As Object, e As EventArgs) Handles btnScores.Click
         Me.Hide()
         Dim newScoresForm As Scores
@@ -43,6 +49,7 @@
         newScoresForm = Nothing
     End Sub
 
+    'Evènement suivant le clic sur le bouton options qui ouvre la fenêtre de changment des options et cache l'accueil
     Private Sub btn_options_Click(sender As Object, e As EventArgs) Handles btn_options.Click
         Dim NouvOptions As Options = New Options()
         Me.Hide()
