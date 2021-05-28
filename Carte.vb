343b41177b2aa216fc36820690fd46133254751c
flowdevLOL <48724807+flowdevLOL@users.noreply.github.com>
2021-05-28 23:51:21 +0200

    doc

--- Carte.vb
+++ Carte.vb
@@ -1,10 +1,21 @@
-﻿Public Class Carte
+﻿'Classe représentant une carte
+Public Class Carte
+    'Image associée à la carte
     Private image As Image
+    'L'identifiant de la carte
     Private imageValue As Integer
+    'Booléen permet de savoir si la carte est retournée
     Private retournee As Boolean
+    'La picturebox associée à la carte
     Private carte As PictureBox
+    'Le suffixe de l'image
     Private imageName As String
 
+    'Le constructeur d'une carte
+    '@param image Image associée à la carte
+    '@param carte carte associée à la carte
+    '@param imgValue L'identifiant de la carte
+    '@param imageName Le suffixe de l'image
     Public Sub New(image As Image, carte As PictureBox, imgValue As Integer, imageName As String)
         Me.image = image
         Me.retournee = False
@@ -15,19 +26,25 @@
     End Sub
 
 
+    'Procédure permettant de désactiver la carte et de la rendre grise
     Public Sub desactiverCarte()
         carte.Enabled = False
         colorPicToGrey()
     End Sub
 
+    'Fonction récupérant l'identifiant de la carte
+    '@return L'identifiant de la carte
     Public Function getImgValue()
         Return Me.imageValue
     End Function
 
+    'Fonction récupérant l'image de la carte
+    '@return L'image de la carte
     Public Function getImage()
         Return Me.image
     End Function
 
+    'Procédure permettant de remettre l'image arrière de la carte d'une carte
     Public Sub setFaceBackCarte()
         If Not carte.Enabled Then
             Return
@@ -37,6 +54,7 @@
 
     End Sub
 
+    'Procédure permettant de retourner une carte afin d'afficher son image
     Public Sub retournerCarte()
         If Not carte.Enabled Then
             Return
@@ -47,6 +65,7 @@
 
     End Sub
 
+    'Procédure de rendre l'image de la carte en variations de gris
     Private Sub colorPicToGrey()
         Dim pic As Bitmap = New Bitmap(carte.Image)
         Dim gray = New Bitmap(pic.Width, pic.Height)
@@ -62,10 +81,14 @@
         carte.Image = gray
     End Sub
 
+    'Fonction renvoyant l'attribut retournee
+    '@return L'état retourné de la carte
     Public Function getEtatRetournee()
         Return Me.retournee
     End Function
 
+    'Fonction renvoyant la pictureBox associée à la carte
+    '@return La pictureBox associée à la carte
     Public Function getPicBox() As PictureBox
         Return Me.carte
     End Function
--- Memory.vb
+++ Memory.vb
@@ -12,10 +12,13 @@
     Private timerOn As Boolean
     Private gameStopped As Integer = -1
 
+    'Procédure permettant de set le joueur de la classe
+    '@param joueur le joueur à set
     Public Sub setJoueur(joueur As Joueur)
         Me.joueur = joueur
     End Sub
 
+    'Procédure permettant d'appliquer les options au formulaire
     Private Sub applyOptions()
         If optionsValues(0) = 0 Then
             timerOn = False
@@ -25,6 +28,9 @@
         MAX_TIME = optionsValues(1) * 60 + optionsValues(2)
     End Sub
 
+    'Gestion de l'évènement click sur le bouton d'abandon
+    '@param sender la source de l'event
+    '@param e les arguments liés à l'event
     Private Sub btnAbandon_Click(sender As Object, e As EventArgs) Handles btnAbandon.Click
         If MsgBox("Abandonner la partie en cours ?", vbYesNo, "Abandon") = vbYes Then
             joueur.addTime(ticks)
@@ -33,6 +39,9 @@
         End If
     End Sub
 
+    'Gestion du chargement du formulaire
+    '@param sender la source de l'event
+    '@param e les arguments liés à l'event
     Private Sub Memory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim i = 0
         Dim imageName As String = Options.getImageOption()
@@ -61,15 +70,23 @@
 
     End Sub
 
+    'Procédure permettant de mélanger les cartes
     Private Sub shuffleCartes()
         Dim r As Random = New Random()
         rdmCartesTab = rdmCartesTab.OrderBy(Function(a) r.Next()).ToArray
     End Sub
 
+    'Fonction retournant une image 
+    '@param i l'indice de l'image à return
+    '@return l'image
     Private Function getRandomImg(i As Integer) As Integer
         Return rdmCartesTab(i)
     End Function
 
+
+    'Gestion d'un tick du timer
+    '@param sender la source de l'event
+    '@param e les arguments liés à l'event
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
         ticks += 1
         If timerOn Then
@@ -84,6 +101,7 @@
         End If
     End Sub
 
+    'Procédure permettant d'arrêter une game
     Private Sub stopGame()
         Timer.Stop()
         'MsgBox(timeScore)
@@ -99,6 +117,8 @@
         Accueil.Show()
     End Sub
 
+    'Function permettant de savoir si une game est finie
+    '@return true si la game est finie, false dans le cas contraire
     Private Function isGameFinished() As Boolean
         If ticks = MAX_TIME And timerOn Then
             Return True
@@ -112,6 +132,9 @@
     End Function
 
 
+    'Gestion de l'évènement clique sur une carte
+    '@param sender la source de l'event
+    '@param e les arguments liés à l'event
     Private Sub carte11_Click(sender As PictureBox, e As EventArgs) Handles carte11.Click, carte12.Click, carte13.Click, carte14.Click, carte15.Click,
             carte21.Click, carte22.Click, carte23.Click, carte24.Click, carte25.Click,
             carte31.Click, carte32.Click, carte33.Click, carte34.Click, carte35.Click,
@@ -159,6 +182,7 @@
 
     End Sub
 
+    'Procédure permettant de bloquer toutes les cartes retournées
     Private Sub lockAllCartesRetournees()
         For Each carte As Carte In cartesRetournees
             carte.desactiverCarte()
@@ -166,10 +190,14 @@
         cartesRetournees.Clear()
     End Sub
 
+    'Fonction permettant de savoir si les cartes retournées et non bloquées forment un carré
+    '@return true si les cartes forment un carré, false dans le cas contraire
     Private Function areCartesCarre() As Boolean
         Return cartesRetournees.Count = 4
     End Function
 
+    'Fonction permettant de savoir si toutes les cartes retournées sont du même signe
+    '@return true si elles sont du même signe, false dans le cas contraire
     Private Function checkAllCartesRetournees() As Boolean
         Dim carteRef As Integer = cartesRetournees(0).getImgValue()
         For Each carte As Carte In cartesRetournees
@@ -180,6 +208,7 @@
         Return True
     End Function
 
+    'Procédure permettant de retournée toutes les cartes retournées
     Private Sub resetAllCartes()
 
         For Each carte As Carte In cartesRetournees
@@ -189,14 +218,20 @@
 
     End Sub
 
+    'Fonction permettant de récupérer la carte associée à la pictureBox
+    '@param source La picture box pour laquelle on veut retrouver la carte
+    '@return la carte trouvée ou null si rien n'est trouvé
     Private Function getCarte(source As PictureBox) As Carte
         For Each carte As Carte In cartes
             If source.Equals(carte.getPicBox()) Then
                 Return carte
             End If
         Next carte
+        Return Nothing
     End Function
 
+    'Procédure permettant de couper le programme 
+    '@param secondes le nombre de secondes pendant lesquelles il faut couper
     Private Sub wait(secondes As Integer)
         Dim i As Integer
         For i = 1 To secondes
@@ -205,12 +240,17 @@
         Next
     End Sub
 
+    'Procédure permettant de mélanger les cartes
     Private Sub initRdmCartesTab()
         For i = 0 To rdmCartesTab.Length - 1
             rdmCartesTab(i) = i Mod 5
         Next
     End Sub
 
+
+    'Gestion de l'évènement click sur le bouton Pause
+    '@param sender la source de l'event
+    '@param e les arguments liés à l'event
     Private Sub PauseButton_Click(sender As Object, e As EventArgs) Handles PauseButton.Click
         If gameStopped = -1 Then
             MsgBox("Impossible de mettre une partie en pause si elle n'est pas commencée ou finie", vbOKOnly, "Pause")
